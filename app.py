fbd71ecfa1a7e140de3048a469bfd03aff31362a
DIEGOGuerrero12 <160614103+DIEGOGuerrero12@users.noreply.github.com>
2024-11-12 02:54:06 +0000

    CREATED configuracion inicial

--- app.py
+++ app.py
@@ -13,3 +13,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/v1/{texto}")
+def mostrar_chat(texto:str):
+    respuesta = respuesta_ia(texto)
+    return {"Respuesta": respuesta}
